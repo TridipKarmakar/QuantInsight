7d8cfe3c3ca2eb06f58b9beb92491df3658fe185
Tridip Karmakar <karmakartridip97@gmail.com>
2026-03-18 12:50:08 +0000

    KAN-34: create parsing funciton in the main.py and checked

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,26 @@
 from api.market_data import fetch_stock_data
 
-if __name__ == "__main__" :
+def parse_stock_data(data) :
+    """
+    Extract time serise data from API response
+    """
 
-    symbol = "AAPL"
+    if "Time Series (Daily)" not in data:
+        raise Exception("Time Series data not found in API response")
+    
+    return data["Time Series (Daily)"]
+
+
+if __name__ == "__main__" : #only run when explicitly run by the name main.py
+
+    symbol = "AAPL" # for now fixed th symbol name lateron change from the browser
 
     try:
-        data = fetch_stock_data(symbol)
-        print("Data fetched successfully ✅")
-        print(data.keys())
+        data = fetch_stock_data(symbol)  #get the overall stock data 
+        time_series = parse_stock_data(data) #get the time series data 
+        
+        print("Parsing successfully ✅")
+        print(list(time_series.keys())[:5])
 
     except Exception as e:
         print("Error:", e)
