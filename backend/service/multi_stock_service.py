aa0d5a90f78ccabd3eb10c2ba0bd89c6b3fe4490
Tridip Karmakar <karmakartridip97@gmail.com>
2026-04-02 09:09:53 +0000

    KAN-50 update multi-stock-service sheet

--- backend/service/multi_stock_service.py
+++ backend/service/multi_stock_service.py
@@ -10,11 +10,18 @@ def get_multi_stock_df(symbols):
 
         df = pd.DataFrame(data)
         df["data"] = pd.to_datetime(df["date"])
-        df = df.sort_values("date")
+        df = df.sort_values(by = "date", ascending=False )
 
-        df = df[["date", "return"]]
+        df = df[["return","date"]]
         df.rename(columns={"return" : symbol}, inplace=True)
 
         all_data.append(df)
 
     final_df = all_data[0]
+
+    for df in all_data[1:]:
+        final_df = pd.merge(final_df,df, on="date", how="inner")
+
+    final_df.set_index("date", inplace=True)
+
+    return final_df
