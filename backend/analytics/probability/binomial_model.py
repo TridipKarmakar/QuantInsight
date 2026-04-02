37f7b4551ff30f448783bc1882d944e0d47a405b
Tridip Karmakar <karmakartridip97@gmail.com>
2026-04-02 11:23:25 +0000

    KAN-51-create-binomial-model

---
+++ backend/analytics/probability/binomial_model.py
@@ -0,0 +1,16 @@
+from probability.binomial import compute_binomial
+import math
+
+def binomial_probability(n, k, p):
+
+    """
+    n = total trials (days)
+    k = success (updays)
+    p = probability of success 
+
+    """
+
+
+    comb = math.comb(n, k)
+
+    return comb * (p ** k) * ((1-p) ** (n-k))  
