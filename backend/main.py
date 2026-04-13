726c2b440f7b12087823d005fac2c3e650ab83d5
Tridip Karmakar <karmakartridip97@gmail.com>
2026-04-13 19:29:14 +0000

    backend deployment ready

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://probable-system-p47jjq5x64xcrgwg-5173.app.github.dev"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
